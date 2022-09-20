36e2d17191d7a800860254d69736b9027d190233
Renato Roessler <renato@dotse.com.br>
2022-09-19 21:09:01 -0300

    feat: remove autofill inpit on chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,9 +4,12 @@ import Styles from './input-styles.scss'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const enabledInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={enabledInput} />
       <span className={Styles.status}>🔴</span>
     </div>
   )
