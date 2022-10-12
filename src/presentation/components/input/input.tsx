250ec692adb0419ffef7b86064168b9a3b90135c
Renato Roessler <renato@dotse.com.br>
2022-10-12 16:49:04 -0300

    feat: ensure login start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
   const enabledInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const getState = (): string => {
+    return '🔴'
+  }
+  const getTitle = (): string => {
+    return error
   }
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly onFocus={enabledInput} />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getState()}</span>
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -8,17 +8,19 @@ import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-};
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({ isLoading: false, errorMessage: '' })
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    main: '',
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório'
+  })
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
@@ -27,7 +29,12 @@ const Login: React.FC = () => {
             name="password"
             placeholder="Digite seu senha"
           />
-          <button data-testid="submit" disabled type="submit" className={Styles.submit}>
+          <button
+            data-testid="submit"
+            disabled
+            type="submit"
+            className={Styles.submit}
+          >
             Entrar
           </button>
           <span className={Styles.link}>Criar conta</span>
