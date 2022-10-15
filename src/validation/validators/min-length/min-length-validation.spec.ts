c166cd21793d440292851b8dd524acdd47393196
Renato Roessler <renato@dotse.com.br>
2022-10-15 18:42:52 -0300

    test: ensure validationComposite return Falsy if there is no error

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,6 +1,5 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validation'
-
 import { faker } from '@faker-js/faker'
 
 const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
