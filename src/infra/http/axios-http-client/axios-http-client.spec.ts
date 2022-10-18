e2b0aba2c48be6c4960f29572087e2fe04289f70
Renato Roessler <renato@dotse.com.br>
2022-10-17 21:15:42 -0300

    fix: change AxiosHttpClient top handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 import { mockPostRequest } from '@/data/test'
-import { mockAxios } from '@/infra/test'
+import { mockAxios, mockHttpResponse } from '@/infra/test'
 import axios from 'axios'
 import { AxiosHttpClient } from './axios-http-client'
 
@@ -29,4 +29,13 @@ describe('AxiosHttpClient', () => {
     const promise = sut.post(mockPostRequest())
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
   })
+
+  // test('Shoudl return the correct statusCode and body on failure', () => {
+  //   const { sut, mockedAxios } = makeSut()
+  //   mockedAxios.post.mockRejectedValueOnce({
+  //     reponse: mockHttpResponse()
+  //   })
+  //   const promise = sut.post(mockPostRequest())
+  //   expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  // })
 })
--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -1,11 +1,13 @@
-import axios from 'axios'
 import { faker } from '@faker-js/faker'
+import axios from 'axios'
+
+export const mockHttpResponse = (): any => ({
+  data: faker.datatype.json(),
+  status: faker.random.alphaNumeric()
+})
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>
-  mockedAxios.post.mockResolvedValue({
-    data: faker.datatype.json(),
-    status: faker.random.alphaNumeric()
-  })
+  mockedAxios.post.mockResolvedValue(mockHttpResponse())
   return mockedAxios
 }
