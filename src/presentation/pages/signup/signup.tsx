601081ec15ffa2805fd910ac071857975e3a1132
Renato Roessler <renato@dotse.com.br>
2022-10-25 17:32:42 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -4,16 +4,29 @@ import {
   LoginHeader
 } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Styles from './signup-styles.scss'
 
 const SignUp: React.FC = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    // name: '',
+    // email: '',
+    // password: '',
+    // passwordConfirmation: '',
+    passwordConfirmationError: 'Campo obrigatório',
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    mainError: ''
+  })
+
   const navigate = useNavigate()
   return (
     <div className={Styles.signup}>
       <LoginHeader />
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state }}>
         <form data-testid="form" className={Styles.form} >
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu Nome" />
@@ -32,6 +45,7 @@ const SignUp: React.FC = () => {
             data-testid="submit"
             type="submit"
             className={Styles.submit}
+            disabled
           >
             Entrar
           </button>
