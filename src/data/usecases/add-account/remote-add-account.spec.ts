ef799214ef764c416d1c5d22c351de59d210fb80
Renato Roessler <renato@dotse.com.br>
2022-10-25 09:37:14 -0300

    test: esnure RemoteAddAcount thrown UnexpectedError if HttPostClient return 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -48,4 +48,11 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAccountParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = { statusCode: HttpStatusCode.serverError }
+    const promise = sut.add(mockAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
