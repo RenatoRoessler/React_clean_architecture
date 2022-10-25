691c3c22b76312b8d9be8a0053076e0489b81167
Renato Roessler <renato@dotse.com.br>
2022-10-25 20:16:43 -0300

    feat: ensure SignUp call AddAccount with correct values

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import { AddAccount } from '@/domain/usecases'
 import {
-  Footer, FormStatus, Input,
+  Footer,
+  FormStatus,
+  Input,
   LoginHeader
 } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
@@ -11,9 +14,10 @@ import Styles from './signup-styles.scss'
 
 type Props = {
   validation: Validation
+  addAccount: AddAccount
 }
 
-const SignUp: React.FC<Props> = ({ validation }: Props) => {
+const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -33,7 +37,10 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password),
-      passwordConfirmationError: validation.validate('passwordConfirmation', state.passwordConfirmation)
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation
+      )
     })
   }, [state.name, state.email, state.password, state.passwordConfirmation])
 
@@ -44,13 +51,23 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
   ): Promise<void> => {
     event.preventDefault()
     setState({ ...state, isLoading: true })
+    await addAccount.add({
+      name: state.name,
+      email: state.email,
+      password: state.password,
+      passwordConfirmation: state.passwordConfirmation
+    })
   }
 
   return (
     <div className={Styles.signup}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu Nome" />
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
@@ -68,11 +85,18 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
             data-testid="submit"
             type="submit"
             className={Styles.submit}
-            disabled={!!state.emailError || !!state.passwordError || !!state.nameError || !!state.passwordConfirmationError}
+            disabled={
+              !!state.emailError ||
+              !!state.passwordError ||
+              !!state.nameError ||
+              !!state.passwordConfirmationError
+            }
           >
             Entrar
           </button>
-          <span className={Styles.link} onClick={() => navigate('/login')} >Voltar para Login</span>
+          <span className={Styles.link} onClick={() => navigate('/login')}>
+            Voltar para Login
+          </span>
           <FormStatus />
         </form>
       </Context.Provider>
