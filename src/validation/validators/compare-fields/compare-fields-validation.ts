700c36ff0c0812df4f517d501830d0d044675f4f
Renato Roessler <renato@dotse.com.br>
2022-10-25 09:59:31 -0300

    feat: esnure ComparefieldsValidation return erro if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,12 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class CompareFieldsValidation implements FieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError(this.field)
+  }
+}
