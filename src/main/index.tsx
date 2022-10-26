690729ae34812c66f75ca8582b588131d79875ca
Renato Roessler <renato@dotse.com.br>
2022-10-26 19:27:42 -0300

    feat: add SignUpComposition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
-ReactDOM.render(<Router MakeLogin={makeLogin} />, document.getElementById('main'))
+ReactDOM.render(<Router MakeLogin={makeLogin} MakeSignUp={makeSignUp} />, document.getElementById('main'))
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,17 +1,17 @@
-import { SignUp } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-type props = {
+type Factory = {
   MakeLogin: React.FC
+  MakeSignUp: React.FC
 }
 
-const Router: React.FC<props> = ({ MakeLogin }) => {
+const Router: React.FC<Factory> = (factory: Factory) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<MakeLogin />} />
-        {/* <Route path="/signup" element={<SignUp />} /> */}
+        <Route path="/login" element={<factory.MakeLogin />} />
+        <Route path="/signup" element={<factory.MakeSignUp />} />
       </Routes>
     </BrowserRouter>
   )
