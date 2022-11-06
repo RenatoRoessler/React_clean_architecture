e2f048cb5750ac9748e3607680d4bfa76d6720fd
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-06 20:24:05 -0300

    test: ensure SignUp present correct error if server fails

--- src/main/test/cypress/support/SIGNUP-mocks.ts
+++ src/main/test/cypress/support/SIGNUP-mocks.ts
@@ -2,3 +2,4 @@ import { faker } from '@faker-js/faker'
 import * as Helper from './http-mock'
 
 export const mockIEmailInUseError = (): void => Helper.mockIEmailInUseError(/signup/)
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
