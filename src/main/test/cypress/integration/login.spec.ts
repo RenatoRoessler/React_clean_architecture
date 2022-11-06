ba9638978f39bc1a885794fccbbef1bc67713c09
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-06 20:31:46 -0300

    refactor: remover duplicated code

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -2,9 +2,12 @@ import { faker } from '@faker-js/faker'
 import * as FormHelper from '../support/form-helper'
 import * as Http from '../support/login-mocks'
 
-const simulateValidSubmit = (): void => {
+const populateFields = (): void => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
+}
+const simulateValidSubmit = (): void => {
+    populateFields()
     cy.getByTestId('submit').click()
 }
 
@@ -72,8 +75,7 @@ describe('Login', () => {
 
     it('Should prevent multiple submits', () => {
         Http.mockOk()
-        cy.getByTestId('email').focus().type(faker.internet.email())
-        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
+        populateFields()
         cy.getByTestId('submit').dblclick()
         FormHelper.testHttpCallsCount(1)
     })
