64552cdb4f72de7c77f107de35aefecceaf6469c
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-06 20:27:55 -0300

    test: ensure signup save accessToken on localStarege on sucess

--- src/main/test/cypress/support/SIGNUP-mocks.ts
+++ src/main/test/cypress/support/SIGNUP-mocks.ts
@@ -4,3 +4,4 @@ import * as Helper from './http-mock'
 export const mockIEmailInUseError = (): void => Helper.mockIEmailInUseError(/signup/)
 export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
 export const mockInvalidData = (): void => Helper.mockOk(/signup/, 'POST', {invalid: faker.random.alphaNumeric(32)})
+export const mockOk = (): void => Helper.mockOk(/signup/, 'POST', {accessToken: faker.random.alphaNumeric(32)})
