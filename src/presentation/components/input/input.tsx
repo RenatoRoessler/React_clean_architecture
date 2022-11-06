13ea0d02f5247c240f9240702228da7ba6f9e7b0
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-06 18:00:18 -0300

    refactor: add input animations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,33 +1,41 @@
-import React, { useContext } from 'react'
-import Styles from './input-styles.scss'
-import Context from '@/presentation/contexts/form/form-context'
+import React, { useContext, useRef } from "react";
+import Styles from "./input-styles.scss";
+import Context from "@/presentation/contexts/form/form-context";
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type Props = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { state, setState } = useContext(Context)
-  const error = state[`${props.name}Error`]
-  const enabledInput = (event: React.FocusEvent<HTMLInputElement>): void => {
-    event.target.readOnly = false
-  }
-  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
-    })
-  }
-  const getState = (): string => {
-    return error ? '🔴' : '🟢'
-  }
-  const getTitle = (): string => {
-    return error || 'Tudo certo!'
-  }
+  const { state, setState } = useContext(Context);
+  const error = state[`${props.name}Error`];
+  const inputRef = useRef<HTMLInputElement>();
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} data-testid={props.name} readOnly onFocus={enabledInput} onChange={handleChange} />
-      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getState()}</span>
+      <input
+        {...props}
+        ref={inputRef}
+        placeholder=" "
+        data-testid={props.name}
+        readOnly
+        onFocus={e => {e.target.readOnly = false}}
+        onChange={e => {setState({
+          ...state,
+          [e.target.name]: e.target.value,
+        })}}
+      />
+      <label onClick={() => inputRef.current.focus()}>{props.placeholder}</label>
+      <span
+        data-testid={`${props.name}-status`}
+        title={error || "Tudo certo!"}
+        className={Styles.status}
+      >
+        {error ? "🔴" : "🟢"}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
