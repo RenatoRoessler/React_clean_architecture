bbdc5e6ad31e777d87903d62e1bc8d270ef92a6f
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-21 19:15:45 -0300

    feat: ensure RemoteLoadSurveyList throws UnexpectedError on 500

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -40,4 +40,11 @@ describe('RemoteLoadSurveyList', () => {
         const promise = sut.loadAll()
         await expect(promise).rejects.toThrow(new UnexpectedError())
     })
+
+    test('Should throw UnexpectedError if HttpGetClient returns 500', async () => {
+        const { sut, httpGetClientSpy } = makeSut()
+        httpGetClientSpy.response = { statusCode: HttpStatusCode.serverError }
+        const promise = sut.loadAll()
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
 })
