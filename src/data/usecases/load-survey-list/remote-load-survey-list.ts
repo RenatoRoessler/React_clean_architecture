ada9c78fde7ca801bf0d6d5a30124c236bb64cdc
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-21 19:25:53 -0300

    feat: ensure RemoteLoadSurveyList return a list on 200

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,15 +1,17 @@
 import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
 import { UnexpectedError } from "@/domain/errors";
+import { SurveyModel } from "@/domain/models";
+import { LoadSurveyList } from "@/domain/usecases";
 
-export class RemoteLoadSurveyList {
+export class RemoteLoadSurveyList implements LoadSurveyList {
     constructor(
         private readonly url: string,
-        private readonly httpGetClient: HttpGetClient) { }
+        private readonly httpGetClient: HttpGetClient<SurveyModel[]>) { }
 
-    async loadAll(): Promise<void> {
+    async loadAll(): Promise<SurveyModel[]> {
         const httpReponse = await this.httpGetClient.get({ url: this.url })
         switch (httpReponse.statusCode) {
-            case HttpStatusCode.ok: break //return httpReponse.body
+            case HttpStatusCode.ok: return httpReponse.body
             // case HttpStatusCode.forbidden: throw new EmailInUseError()
             default: throw new UnexpectedError()
         }
