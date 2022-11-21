5a1274f841cc87f0c391578421c7254e99e58a0f
Renato Roessler <renato.roessler@pro22.com.br>
2022-11-21 19:13:41 -0300

    feat: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,14 +1,17 @@
+import { HttpStatusCode } from "@/data/protocols/http";
 import { HttpGetClientSpy } from "@/data/test";
+import { UnexpectedError } from "@/domain/errors";
+import { SurveyModel } from "@/domain/models";
 import { faker } from "@faker-js/faker";
 import { RemoteLoadSurveyList } from "./remote-load-survey-list";
 
 type SutTypes = {
     sut: RemoteLoadSurveyList;
-    httpGetClientSpy: HttpGetClientSpy;
+    httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>;
 }
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-    const httpGetClientSpy = new HttpGetClientSpy()
+    const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
     const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
     return {
         sut,
@@ -23,4 +26,11 @@ describe('RemoteLoadSurveyList', () => {
         await sut.loadAll()
         expect(httpGetClientSpy.url).toBe(url)
     })
+
+    test('Should throw UnexpectedError if HttpGetClient returns 403', async () => {
+        const { sut, httpGetClientSpy } = makeSut()
+        httpGetClientSpy.response = { statusCode: HttpStatusCode.forbidden }
+        const promise = sut.loadAll()
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+      })
 })
--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,4 +1,5 @@
-import { HttpGetClient } from "@/data/protocols/http";
+import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
+import { UnexpectedError } from "@/domain/errors";
 
 export class RemoteLoadSurveyList {
     constructor(
@@ -6,6 +7,11 @@ export class RemoteLoadSurveyList {
         private readonly httpGetClient: HttpGetClient) { }
 
     async loadAll(): Promise<void> {
-        await this.httpGetClient.get({ url: this.url })
+        const httpReponse = await this.httpGetClient.get({ url: this.url })
+        switch (httpReponse.statusCode) {
+            case HttpStatusCode.ok: break //return httpReponse.body
+            // case HttpStatusCode.forbidden: throw new EmailInUseError()
+            default: throw new UnexpectedError()
+        }
     }
 }
